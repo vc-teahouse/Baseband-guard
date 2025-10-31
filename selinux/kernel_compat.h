92b86d825c613af9081c7699c45c95edbca08da7
AlexLiuDev233 <wzylin11@outlook.com>
2025-10-31 20:49:52 +0800

    fix: build failed in 6.4-5.6

--- selinux/kernel_compat.h
+++ selinux/kernel_compat.h
@@ -25,7 +25,7 @@ static inline bool selinux_initialized_compat()
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6,4,0)
     return selinux_initialized();
 #elif LINUX_VERSION_CODE >= KERNEL_VERSION(5,6,0)
-    return selinux_initialized(selinux_state);
+    return selinux_initialized(&selinux_state);
 #else
     return selinux_state.initialized;
 #endif
