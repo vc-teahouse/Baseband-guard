a5cde74d74dc0aaf193add49022db9470a7cc6d4
cctv18 <85936817+cctv18@users.noreply.github.com>
2025-12-18 04:57:08 +0800

    Refactor fmt & fix string escaping bug (#67)
    - [x] [Converted DOS format to Unix
    format](https://github.com/vc-teahouse/Baseband-guard/pull/67/commits/2e32611e33ada7dc07674daa79db58fedffc342a)
    .
    - [x] Unified the incoming variables into strings in Makefile instead of
    converting formats in C code, avoiding incomplete link display in
    bootlog caused by escaping double slashes in the incoming links as
    comment symbols.

--- baseband_guard.c
+++ baseband_guard.c
@@ -325,8 +325,8 @@ static int __init bbg_init(void)
 {
 	security_add_hooks_compat(bb_hooks, ARRAY_SIZE(bb_hooks));
 	pr_info("baseband_guard power by https://t.me/qdykernel\n");
-	pr_info("baseband_guard repo: %s", __stringify(BBG_REPO));
-	pr_info("baseband_guard version: %s", __stringify(BBG_VERSION));
+	pr_info("baseband_guard repo: %s", BBG_REPO);
+	pr_info("baseband_guard version: %s", BBG_VERSION);
 	return 0;
 }
 
