415d0eb1f806576ed1f498e8b26e6887c92f5105
YC酱luyancib <luyancib@qq.com>
2026-02-07 14:18:55 +0800

    detect selinux_state

--- tracing/kernel_compat.h
+++ tracing/kernel_compat.h
@@ -10,13 +10,15 @@
 
 static __maybe_unused inline bool selinux_initialized_compat(void)
 {
+#ifdef BB_HAS_SELINUX_STATE
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6,4,0)
     return selinux_initialized();
 #elif LINUX_VERSION_CODE >= KERNEL_VERSION(5,6,0)
     return selinux_initialized(&selinux_state);
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4,17,0)
+#else // LINUX_VERSION_CODE
     return selinux_state.initialized;
-#else
+#endif // LINUX_VERSION_CODE
+#else // BB_HAS_SELINUX_STATE
     return ss_initialized;
-#endif
+#endif // BB_HAS_SELINUX_STATE
 }
